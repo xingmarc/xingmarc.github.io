6a8b393bcd50d5b22c46fcb0bf999dba2387c433
Xing <xingmarc@yeah.net>
2016-02-01 19:48:46 -0800

    add more cases and change some html style

--- js/haha.js
+++ js/haha.js
@@ -13,12 +13,27 @@
 		var parseAndSay = function(question){
 			if (question.indexOf("你好") > -1){
 				$scope.eldersResponse = "Excited!";
+				return;
 			}
 			if ( (question.indexOf("董先生") > -1 || question.indexOf("董建华") > -1) && question.indexOf("支持") > -1){
 				$scope.eldersResponse = "你问我滋瓷不滋瓷，我是滋瓷滴~";
+				return;
+			}
+			if (question.indexOf("华莱士") > -1){
+				$scope.eldersResponse = "美国的华莱士比你们不知道高到哪里去了，我和他谈笑风生";
+				return;
+			}
+			if (question.indexOf("为什么") > -1){
+				$scope.eldersResponse = "任何事情都要按照基本法";
+				return;
+			}
+			if (question.indexOf("宋祖英") > -1 || question.indexOf("国母") > -1){
+				$scope.eldersResponse = "I'm Angry!";
+				return;
 			}
 			else{
 				$scope.eldersResponse = "无可奉告";
+				return;
 			}
 		}
 
