b5c846c30273c7ce3adebed00ebd0bb2105c75ce
Xing <xingmarc@yeah.net>
2016-02-08 15:45:34 -0800

    add state provider

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 'use strict';
 
-angular.module('HahaX',[]);
+angular.module('HahaX',[])
+.config(function($locationProvider,$stateProvider, $urlRouterProvider,localStorageServiceProvider) {
 
-$stateProvider
-.state('index',{
-  	url: '',
- 	abstract: true,
-  	views: {
-    	'header': {
-      		templateUrl: 'views/header.html',
-      		controller: 'headerCtrl'
-    	}
-  }
-})
-.state('index.haha', {
-  	url: "/haha",
-  	views: {
-    	'container@': {
-      		templateUrl: "views/haha.html",
-      		controller: 'hahaHomepageCtrl'
-  		}
-  	}
-})
+	$stateProvider
+	.state('index',{
+	  	url: '',
+	 	abstract: true,
+	  	views: {
+	    	'header': {
+	      		templateUrl: 'views/header.html',
+	      		controller: 'headerCtrl'
+	    	}
+	  }
+	})
+	.state('index.haha', {
+	  	url: "/haha",
+	  	views: {
+	    	'container@': {
+	      		templateUrl: "views/haha.html",
+	      		controller: 'hahaHomepageCtrl'
+	  		}
+	  	}
+	})
+
+});
