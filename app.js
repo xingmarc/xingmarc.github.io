22a134f4b19e242099cbdf81639cbd6bbf44e766
Xing <xingmarc@yeah.net>
2016-02-08 16:27:53 -0800

    add a test button

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 angular.module('HahaX',['ui.router'])
 .config(function($stateProvider,$urlRouterProvider) {
-	
+
 	$urlRouterProvider.otherwise("/");
 	$stateProvider
 	.state('index',{
@@ -16,13 +16,13 @@ angular.module('HahaX',['ui.router'])
 	  }
 	})
 	.state('index.landing', {
-      url: "/",
-      views: {
-        'container@': {
-          templateUrl: "views/landing.html",
-          controller: 'landingCtrl',
-        }
-      }
+      	url: "/",
+      	views: {
+        	'container@': {
+          		templateUrl: "views/landing.html",
+          		controller: 'landingCtrl',
+        	}
+      	}
     })
 	.state('index.haha', {
 	  	url: "/haha",
