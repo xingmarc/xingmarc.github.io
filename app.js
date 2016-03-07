b1de25e53d9b4657b68ec78aca3c69f530067e5c
Xing <xingmarc@yeah.net>
2016-03-07 00:32:19 -0800

    change the state config

--- app.js
+++ app.js
@@ -4,31 +4,31 @@ angular.module('HahaX',['ui.router'])
 .config(function($stateProvider,$urlRouterProvider) {
 
 	$urlRouterProvider.otherwise("/");
-	$urlRouterProvider.when('/root','/home/index');
+	//$urlRouterProvider.when('/root','/home/index');
 	$stateProvider
-	// .state('root', {
- //    	url: '',
- //    	//abstract: true,
- //    	templateUrl: "views/home.html",
- //    	controller: "homeCtrl"
+	.state('root', {
+    	url: '',
+    	abstract: true,
+    	templateUrl: 'views/home.html',
+    	controller: 'homeCtrl'
 
- //    	/*
- //    	template: "views/root.html",
- //    	controller: 'rootCtrl'*/
-	// })
+    	/*
+    	template: "views/root.html",
+    	controller: 'rootCtrl'*/
+	})
 	.state('root.home',{
-		url:'/home',
-		templateUrl: "views/home.html",
-    	controller:'homeCtrl'
+		url:'',
+		templateUrl: 'views/home.html',
+    	controller: 'homeCtrl'
 	})
 	.state('root.haha', {
-	  	url: "/haha",
-	    templateUrl: "views/haha.html",
+	  	url: '/haha',
+	    templateUrl: 'views/haha.html',
 	    controller: 'hahaHomepageCtrl'
 	})
 	.state('root.hex',{
-		url:"/hex",
-		templateUrl:"views/hex.html",
+		url:'/hex',
+		templateUrl:'views/hex.html',
 		controller:'hexCtrl'
 	})
 
