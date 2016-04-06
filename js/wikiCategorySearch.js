4a1efe1d525bef2cc382432f5bd28afd967c23f5
Xing <xingmarc@yeah.net>
2016-04-06 00:45:29 -0700

    typo

--- js/wikiCategorySearch.js
+++ js/wikiCategorySearch.js
@@ -58,7 +58,7 @@ $.ajax( {
 
     			console.log(item);
 
-    			console.log(response.data.query.page[item]);
+    			console.log(response.data.query.pages[item]);
     			$scope.categories.push(response.data.query.pages.item);
     		})
 
