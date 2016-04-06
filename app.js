e325b29848be6a5f736c9d757e61ea0aadce4785
Xing <xingmarc@yeah.net>
2016-04-06 01:23:29 -0700

    add ng-animate

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('HahaX',['ui.router'])
+angular.module('HahaX',['ui.router','ngAnimate'])
 .config(function($stateProvider,$urlRouterProvider,$httpProvider) {
 
 	//$urlRouterProvider.otherwise("/");
