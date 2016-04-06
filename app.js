07f79b76f1018102acb08e9831072f353543e82a
Xing <xingmarc@yeah.net>
2016-04-05 18:00:41 -0700

    try use http get

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('HahaX',['ui.router','ngResource'])
+angular.module('HahaX',['ui.router'])
 .config(function($stateProvider,$urlRouterProvider) {
 
 	//$urlRouterProvider.otherwise("/");
--- js/wikiCategorySearch.js
+++ js/wikiCategorySearch.js
@@ -4,16 +4,37 @@
 	angular.module('HahaX')
 	.controller('wikiCategorySearchCtrl',wikiCategorySearchCtrl);
 
-	function wikiCategorySearchCtrl($scope, wikiAPI){
+	function wikiCategorySearchCtrl($scope, $http, WIKI_API_URL){
 
 
 
 		$scope.aaa = "yoyo";
 
-		var test = wikiAPI.get();
 
-		console.log(test);
 
+		// var test = wikiAPI.get();
+
+		// console.log(test);
+
+		$http({
+  			method: 'GET',
+  			url: WIKI_API_URL
+		}).then(function successCallback(response) {
+    		// this callback will be called asynchronously
+    		// when the response is available
+
+    		aaa="good";
+
+    		console.log(response);
+
+  		}, function errorCallback(response) {
+    		// called asynchronously if an error occurs
+    		// or server returns response with an error status.
+
+    		aaa = "bad";
+
+    		console.log(response);
+  		});
 
 	}
 
